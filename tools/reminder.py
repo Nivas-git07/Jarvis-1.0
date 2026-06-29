d57d9f7f54688916ecff94e17b00b127de917914
Nivas-git07 <itsmenivas007@gmail.com>
2026-06-29 18:04:01 +0000

    add schedule feature

---
+++ tools/reminder.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+FILE = "data/reminders.json"
+
+def load_reminders():
+    if not os.path.exists(FILE):
+        return []
+
+    with open(FILE, "r") as f:
+        return json.load(f)
+
+def save_reminder(title, datetime_str):
+    reminders = load_reminders()
+
+    reminders.append({
+        "title": title,
+        "datetime": datetime_str,
+        "completed": False
+    })
+
+    with open(FILE, "w") as f:
+        json.dump(reminders, f, indent=4)
+
+def get_reminders():
+    return load_reminders()
